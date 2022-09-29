b8eba695e8e294627ec3643a23a179aafd0fc67f
yanagitani <takanori.yanagitani@gmail.com>
2022-09-29 09:02:08 +0900

    test: more coverage

--- next_test.go
+++ next_test.go
@@ -1,6 +1,8 @@
 package fsring
 
 import (
+	"errors"
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -78,32 +80,82 @@ func TestNext(t *testing.T) {
 		t.Run("FallbackIfNotEmpty", func(t *testing.T) {
 			t.Parallel()
 
-			var root string = filepath.Join(ITEST_FSRING_DIR, "Next/FallbackIfNotEmpty")
-
-			e := os.MkdirAll(root, 0755)
+			e := os.RemoveAll(filepath.Join(ITEST_FSRING_DIR, "Next/FallbackIfNotEmpty"))
 			mustNil(e)
 
-			var chk NameChecker = NameCheckerNoCheck
-			var ie IsEmpty = IsEmptyBuilderNew(chk)
+			t.Run("NextBuilderHeavy4", func(t *testing.T) {
+				t.Parallel()
+				var root string = filepath.Join(ITEST_FSRING_DIR, "Next/FallbackIfNotEmpty/16")
 
-			var mng string = "manage.txt"
-			var wrongNext string = filepath.Join(root, "0000")
+				e := os.MkdirAll(root, 0755)
+				mustNil(e)
 
-			e = os.WriteFile(filepath.Join(root, mng), []byte(wrongNext), 0755)
-			mustNil(e)
+				var chk NameChecker = NameCheckerNoCheck
+				var ie IsEmpty = IsEmptyBuilderNew(chk)
 
-			var n Next = NextBuilderNew(chk)(root)(mng).
-				FallbackIfNotEmpty(ie, NextBuilderHeavy4(ie))
+				var mng string = "manage.txt"
+				var wrongNext string = filepath.Join(root, "0000")
 
-			f, e := os.Create(wrongNext)
-			mustNil(e)
-			_, e = f.Write([]byte("not empty"))
-			mustNil(e)
-			f.Close()
+				e = os.WriteFile(filepath.Join(root, mng), []byte(wrongNext), 0755)
+				mustNil(e)
 
-			nex, e := n(root)
-			mustNil(e)
-			t.Run("Must be same", check(nex, filepath.Join(root, "0001")))
+				var n Next = NextBuilderNew(chk)(root)(mng).
+					FallbackIfNotEmpty(ie, NextBuilderHeavy4(ie))
+
+				f, e := os.Create(wrongNext)
+				mustNil(e)
+				_, e = f.Write([]byte("not empty"))
+				mustNil(e)
+				f.Close()
+
+				nex, e := n(root)
+				mustNil(e)
+				t.Run("Must be same", check(nex, filepath.Join(root, "0001")))
+			})
+
+			t.Run("NextBuilderHeavy3", func(t *testing.T) {
+				t.Parallel()
+				var root string = filepath.Join(ITEST_FSRING_DIR, "Next/FallbackIfNotEmpty/8")
+
+				e := os.MkdirAll(root, 0755)
+				mustNil(e)
+
+				var chk NameChecker = NameCheckerNoCheck
+				var ie IsEmpty = IsEmptyBuilderNew(chk)
+
+				var mng string = "manage.txt"
+				var wrongNext string = filepath.Join(root, "00")
+
+				e = os.WriteFile(filepath.Join(root, mng), []byte(wrongNext), 0755)
+				mustNil(e)
+
+				var n Next = NextBuilderNew(chk)(root)(mng).
+					FallbackIfNotEmpty(ie, NextBuilderHeavy3(ie))
+
+				f, e := os.Create(wrongNext)
+				mustNil(e)
+				_, e = f.Write([]byte("not empty"))
+				mustNil(e)
+				f.Close()
+
+				nex, e := n(root)
+				mustNil(e)
+				t.Run("Must be same", check(nex, filepath.Join(root, "01")))
+
+				for i := 0; i < 256; i++ {
+					name := filepath.Join(root, fmt.Sprintf("%02x", i))
+					f, e := os.Create(name)
+					mustNil(e)
+					defer f.Close()
+					_, e = f.Write([]byte("not empty"))
+					mustNil(e)
+				}
+
+				_, e = n(root)
+				t.Run("Must fail(too many queue)", check(nil != e, true))
+				t.Run("Must same(too many queue)", check(errors.Is(e, ErrTooManyQueue), true))
+			})
+
 		})
 	})
 }
